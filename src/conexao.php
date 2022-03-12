e286cc863764e50f2e5677c1c9cf6a64c931fd25
TheGBO <83230635+TheGBO@users.noreply.github.com>
2022-03-11 21:47:06 -0300

    Update conexao.php

--- src/conexao.php
+++ src/conexao.php
@@ -1,7 +1,7 @@
 <?php
     define('HOST','127.0.0.1');
     define('USUARIO', 'root');
-    define('SENHA', 'Adtv4465+');
+    define('SENHA', '');
     define('DB', 'kekapp');
 
     $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die('não foi possivel efetuar a conexão');
